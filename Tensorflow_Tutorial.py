00db21a4a72abc79e48bc945bb3e26877fee4fc6
Nick Acosta <nacosta@us.ibm.com>
2018-02-05 15:53:18 -0600

    clean up

--- Tensorflow_Tutorial.py
+++ Tensorflow_Tutorial.py
@@ -1,22 +1,11 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import math
 import numpy as np
 import h5py
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.python.framework import ops
 
 
-get_ipython().magic(u'matplotlib inline')
 np.random.seed(1)
-
-
-# In[17]:
 
 
 def one_hot_matrix(labels, C):
@@ -55,78 +44,20 @@ def one_hot_matrix(labels, C):
     return one_hot
 
 
-# In[4]:
-
-
 mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-
-
-# Change the index below and run the cell to visualize some examples in the dataset.
-
-# In[5]:
-
 
 train_data = mnist.train.images # Returns np.array
 train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
 eval_data = mnist.test.images # Returns np.array
 eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
-
-# In[16]:
-
-
-train_labels[6]
-
-
-# In[14]:
-
-
-pixels = np.array(train_data[10])
-
-# Reshape the array into 28 x 28 array (2-dimensional array)
-pixels = pixels.reshape((28, 28))
-
-# Plot
-
-plt.imshow(pixels, cmap='gray')
-plt.show()
-
-
-# As usual you flatten the image dataset, then normalize it by dividing by 255. On top of that, you will convert each label to a one-hot vector as shown in Figure 1. Run the cell below to do so.
-
-# In[20]:
-
-
 one_hot_train_labels = one_hot_matrix(train_labels, 10)
-
-
-# In[28]:
-
-
-one_hot_train_labels[0][7]
-
-
-# In[45]:
-
-
 X_train = train_data.T
 Y_train = one_hot_train_labels
 X_test = eval_data.T
 Y_test = one_hot_matrix(eval_labels, 10)
 
 print(X_train.shape,Y_train.shape,X_test.shape,Y_test.shape)
-
-
-# ### 2.1 - Create placeholders
-# 
-# Your first task is to create placeholders for `X` and `Y`. This will allow you to later pass your training data in when you run your session. 
-# 
-# **Exercise:** Implement the function below to create the placeholders in tensorflow.
-
-# In[31]:
-
-
-# GRADED FUNCTION: create_placeholders
 
 def create_placeholders(n_x, n_y):
     """
@@ -152,53 +83,6 @@ def create_placeholders(n_x, n_y):
     
     return X, Y
 
-
-# In[47]:
-
-
-X, Y = create_placeholders(X_train.shape[0], Y_train.shape[0])
-print ("X = " + str(X))
-print ("Y = " + str(Y))
-
-
-# **Expected Output**: 
-# 
-# <table> 
-#     <tr> 
-#         <td>
-#             **X**
-#         </td>
-#         <td>
-#         Tensor("Placeholder_1:0", shape=(12288, ?), dtype=float32) (not necessarily Placeholder_1)
-#         </td>
-#     </tr>
-#     <tr> 
-#         <td>
-#             **Y**
-#         </td>
-#         <td>
-#         Tensor("Placeholder_2:0", shape=(10, ?), dtype=float32) (not necessarily Placeholder_2)
-#         </td>
-#     </tr>
-# 
-# </table>
-
-# ### 2.2 - Initializing the parameters
-# 
-# Your second task is to initialize the parameters in tensorflow.
-# 
-# **Exercise:** Implement the function below to initialize the parameters in tensorflow. You are going use Xavier Initialization for weights and Zero Initialization for biases. The shapes are given below. As an example, to help you, for W1 and b1 you could use: 
-# 
-# ```python
-# W1 = tf.get_variable("W1", [25,12288], initializer = tf.contrib.layers.xavier_initializer(seed = 1))
-# b1 = tf.get_variable("b1", [25,1], initializer = tf.zeros_initializer())
-# ```
-# Please use `seed = 1` to make sure your results match ours.
-
-# In[35]:
-
-
-# GRADED FUNCTION: initialize_parameters
 
 def initialize_parameters():
     """
@@ -234,76 +118,6 @@ def initialize_parameters():
     
     return parameters
 
-
-# In[36]:
-
-
-tf.reset_default_graph()
-with tf.Session() as sess:
-    parameters = initialize_parameters()
-    print("W1 = " + str(parameters["W1"]))
-    print("b1 = " + str(parameters["b1"]))
-    print("W2 = " + str(parameters["W2"]))
-    print("b2 = " + str(parameters["b2"]))
-
-
-# **Expected Output**: 
-# 
-# <table> 
-#     <tr> 
-#         <td>
-#             **W1**
-#         </td>
-#         <td>
-#          < tf.Variable 'W1:0' shape=(25, 12288) dtype=float32_ref >
-#         </td>
-#     </tr>
-#     <tr> 
-#         <td>
-#             **b1**
-#         </td>
-#         <td>
-#         < tf.Variable 'b1:0' shape=(25, 1) dtype=float32_ref >
-#         </td>
-#     </tr>
-#     <tr> 
-#         <td>
-#             **W2**
-#         </td>
-#         <td>
-#         < tf.Variable 'W2:0' shape=(12, 25) dtype=float32_ref >
-#         </td>
-#     </tr>
-#     <tr> 
-#         <td>
-#             **b2**
-#         </td>
-#         <td>
-#         < tf.Variable 'b2:0' shape=(12, 1) dtype=float32_ref >
-#         </td>
-#     </tr>
-# 
-# </table>
-
-# As expected, the parameters haven't been evaluated yet.
-
-# ### 2.3 - Forward propagation in tensorflow 
-# 
-# You will now implement the forward propagation module in tensorflow. The function will take in a dictionary of parameters and it will complete the forward pass. The functions you will be using are: 
-# 
-# - `tf.add(...,...)` to do an addition
-# - `tf.matmul(...,...)` to do a matrix multiplication
-# - `tf.nn.relu(...)` to apply the ReLU activation
-# 
-# **Question:** Implement the forward pass of the neural network. We commented for you the numpy equivalents so that you can compare the tensorflow implementation to numpy. It is important to note that the forward propagation stops at `z3`. The reason is that in tensorflow the last linear layer output is given as input to the function computing the loss. Therefore, you don't need `a3`!
-# 
-# 
-
-# In[37]:
-
-
-# GRADED FUNCTION: forward_propagation
-
 def forward_propagation(X, parameters):
     """
     Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
@@ -335,50 +149,6 @@ def forward_propagation(X, parameters):
     
     return Z3
 
-
-# In[39]:
-
-
-tf.reset_default_graph()
-
-with tf.Session() as sess:
-    X, Y = create_placeholders(784, 10)
-    parameters = initialize_parameters()
-    Z3 = forward_propagation(X, parameters)
-    print("Z3 = " + str(Z3))
-
-
-# **Expected Output**: 
-# 
-# <table> 
-#     <tr> 
-#         <td>
-#             **Z3**
-#         </td>
-#         <td>
-#         Tensor("Add_2:0", shape=(6, ?), dtype=float32)
-#         </td>
-#     </tr>
-# 
-# </table>
-
-# You may have noticed that the forward propagation doesn't output any cache. You will understand why below, when we get to brackpropagation.
-
-# ### 2.4 Compute cost
-# 
-# As seen before, it is very easy to compute the cost using:
-# ```python
-# tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = ..., labels = ...))
-# ```
-# **Question**: Implement the cost function below. 
-# - It is important to know that the "`logits`" and "`labels`" inputs of `tf.nn.softmax_cross_entropy_with_logits` are expected to be of shape (number of examples, num_classes). We have thus transposed Z3 and Y for you.
-# - Besides, `tf.reduce_mean` basically does the summation over the examples.
-
-# In[40]:
-
-
-# GRADED FUNCTION: compute_cost 
-
 def compute_cost(Z3, Y):
     """
     Computes the cost
@@ -400,63 +170,6 @@ def compute_cost(Z3, Y):
     ### END CODE HERE ###
     
     return cost
-
-
-# In[41]:
-
-
-tf.reset_default_graph()
-
-with tf.Session() as sess:
-    X, Y = create_placeholders(784, 10)
-    parameters = initialize_parameters()
-    Z3 = forward_propagation(X, parameters)
-    cost = compute_cost(Z3, Y)
-    print("cost = " + str(cost))
-
-
-# **Expected Output**: 
-# 
-# <table> 
-#     <tr> 
-#         <td>
-#             **cost**
-#         </td>
-#         <td>
-#         Tensor("Mean:0", shape=(), dtype=float32)
-#         </td>
-#     </tr>
-# 
-# </table>
-
-# ### 2.5 - Backward propagation & parameter updates
-# 
-# This is where you become grateful to programming frameworks. All the backpropagation and the parameters update is taken care of in 1 line of code. It is very easy to incorporate this line in the model.
-# 
-# After you compute the cost function. You will create an "`optimizer`" object. You have to call this object along with the cost when running the tf.session. When called, it will perform an optimization on the given cost with the chosen method and learning rate.
-# 
-# For instance, for gradient descent the optimizer would be:
-# ```python
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate).minimize(cost)
-# ```
-# 
-# To make the optimization you would do:
-# ```python
-# _ , c = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
-# ```
-# 
-# This computes the backpropagation by passing through the tensorflow graph in the reverse order. From cost to inputs.
-# 
-# **Note** When coding, we often use `_` as a "throwaway" variable to store values that we won't need to use later. Here, `_` takes on the evaluated value of `optimizer`, which we don't need (and `c` takes the value of the `cost` variable). 
-
-# ### 2.6 - Building the model
-# 
-# Now, you will bring it all together! 
-# 
-# **Exercise:** Implement the model. You will be calling the functions you had previously implemented.
-
-# In[59]:
-
 
 def random_mini_batches(X, Y, mini_batch_size, seed):
     """
@@ -500,18 +213,6 @@ def random_mini_batches(X, Y, mini_batch_size, seed):
         mini_batches.append(mini_batch)
     
     return mini_batches
-
-
-# In[61]:
-
-
-Y_train.shape
-
-mini_batches = random_mini_batches(X_train, Y_train, 5000, 0)
-print(mini_batches)
-
-
-# In[62]:
 
 
 def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
@@ -602,11 +303,11 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
                 costs.append(epoch_cost)
                 
         # plot the cost
-        plt.plot(np.squeeze(costs))
-        plt.ylabel('cost')
-        plt.xlabel('iterations (per tens)')
-        plt.title("Learning rate =" + str(learning_rate))
-        plt.show()
+        #plt.plot(np.squeeze(costs))
+        #plt.ylabel('cost')
+        #plt.xlabel('iterations (per tens)')
+        #plt.title("Learning rate =" + str(learning_rate))
+        #plt.show()
 
         # lets save the parameters in a variable
         parameters = sess.run(parameters)
@@ -624,73 +325,4 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
         return parameters
 
 
-# Run the following cell to train your model! On our machine it takes about 5 minutes. Your "Cost after epoch 100" should be 1.016458. If it's not, don't waste time; interrupt the training by clicking on the square (⬛) in the upper bar of the notebook, and try to correct your code. If it is the correct cost, take a break and come back in 5 minutes!
-
-# In[63]:
-
-
 parameters = model(X_train, Y_train, X_test, Y_test)
-
-
-# In[67]:
-
-
-#from zipfile_infolist import print_info
-import zipfile
-
-
-# In[69]:
-
-
-msg = 'This data did not exist in a file before being added to the ZIP file'
-zf = zipfile.ZipFile('zipfile_writestr.zip', 
-                     mode='w',
-                     compression=zipfile.ZIP_DEFLATED, 
-                     )
-try:
-    zf.writestr('from_string.txt', msg)
-finally:
-    zf.close()
-
-
-zf = zipfile.ZipFile('zipfile_writestr.zip', 'r')
-print(zf.read('from_string.txt'))
-
-
-# In[72]:
-
-
-zf = zipfile.ZipFile('tf-softmax-model.zip', 'r')
-
-
-# In[71]:
-
-
-get_ipython().system(u'ls')
-
-
-# In[73]:
-
-
-zf
-
-
-# In[74]:
-
-
-for info in zf.infolist():
-        print(info.filename)
-        print '\tComment:\t', info.comment)
-        print '\tModified:\t', datetime.datetime(*info.date_time))
-        print '\tSystem:\t\t', info.create_system, '(0 = Windows, 3 = Unix)')
-        print '\tZIP version:\t', info.create_version)
-        print '\tCompressed:\t', info.compress_size, 'bytes')
-        print('\tUncompressed:\t', info.file_size, 'bytes')
-        print
-
-
-# In[76]:
-
-
-zf.read('input_data_softmax.py')
-
