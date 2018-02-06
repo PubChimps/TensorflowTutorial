cd49b1c1ef99b7c1de89876942c14e0d4976aca2
Nick Acosta <30478672+PubChimps@users.noreply.github.com>
2018-02-06 13:28:00 -0600

    add mnist to bucket ops

--- Tensorflow_Tutorial.py
+++ Tensorflow_Tutorial.py
@@ -2,10 +2,153 @@ import math
 import numpy as np
 import h5py
 import tensorflow as tf
+import argparse
 from tensorflow.python.framework import ops
 
 np.random.seed(1)
 
+import gzip
+import os
+from six.moves.urllib.request import urlretrieve
+import numpy
+
+def _read32(bytestream):
+    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+
+def extract_images(filename):
+    """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
+    #print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2051:
+            raise ValueError(
+                'Invalid magic number %d in MNIST image file: %s' %
+                (magic, filename))
+        num_images = _read32(bytestream)
+        rows = _read32(bytestream)
+        cols = _read32(bytestream)
+        buf = bytestream.read(rows * cols * num_images)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8)
+        data = data.reshape(num_images, rows, cols, 1)
+    return data
+
+
+def dense_to_one_hot(labels_dense, num_classes=10):
+    """Convert class labels from scalars to one-hot vectors."""
+    num_labels = labels_dense.shape[0]
+    index_offset = numpy.arange(num_labels) * num_classes
+    labels_one_hot = numpy.zeros((num_labels, num_classes))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+
+def extract_labels(filename, one_hot=False):
+    """Extract the labels into a 1D uint8 numpy array [index]."""
+    #print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2049:
+            raise ValueError(
+                'Invalid magic number %d in MNIST label file: %s' %
+                (magic, filename))
+        num_items = _read32(bytestream)
+        buf = bytestream.read(num_items)
+        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+        if one_hot:
+            return dense_to_one_hot(labels)
+    return labels
+
+
+class DataSet(object):
+    def __init__(self, images, labels, fake_data=False):
+        if fake_data:
+            self._num_examples = 10000
+        else:
+            assert images.shape[0] == labels.shape[0], (
+                "images.shape: %s labels.shape: %s" % (images.shape,
+                                                       labels.shape))
+            self._num_examples = images.shape[0]
+            # Convert shape from [num examples, rows, columns, depth]
+            # to [num examples, rows*columns] (assuming depth == 1)
+            assert images.shape[3] == 1
+            images = images.reshape(images.shape[0],
+                                    images.shape[1] * images.shape[2])
+            # Convert from [0, 255] -> [0.0, 1.0].
+            images = images.astype(numpy.float32)
+            images = numpy.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, fake_data=False):
+        """Return the next `batch_size` examples from this data set."""
+        if fake_data:
+            fake_image = [1.0 for _ in xrange(784)]
+            fake_label = 0
+            return [fake_image for _ in xrange(batch_size)], [
+                fake_label for _ in xrange(batch_size)]
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end]
+
+
+def read_data_sets(train_images_file, train_labels_file, test_images_file, test_labels_file, fake_data=False, one_hot=False):
+    class DataSets(object):
+        pass
+    data_sets = DataSets()
+    if fake_data:
+        data_sets.train = DataSet([], [], fake_data=True)
+        data_sets.validation = DataSet([], [], fake_data=True)
+        data_sets.test = DataSet([], [], fake_data=True)
+        return data_sets
+    TRAIN_IMAGES = train_images_file
+    TRAIN_LABELS = train_labels_file
+    TEST_IMAGES = test_images_file
+    TEST_LABELS = test_labels_file
+    VALIDATION_SIZE = 5000
+    train_images = extract_images(TRAIN_IMAGES)
+    train_labels = extract_labels(TRAIN_LABELS, one_hot=one_hot)
+    test_images = extract_images(TEST_IMAGES)
+    test_labels = extract_labels(TEST_LABELS, one_hot=one_hot)
+    data_sets.train = DataSet(train_images[VALIDATION_SIZE:], train_labels[VALIDATION_SIZE:])
+    data_sets.validation = DataSet(train_images[:VALIDATION_SIZE], train_labels[:VALIDATION_SIZE])
+    data_sets.test = DataSet(test_images, test_labels)
+    return data_sets
+
+
+#####TF CODE
 def one_hot_matrix(labels, C):
     """
     Creates a matrix where the i-th row corresponds to the ith class number and the jth column
@@ -306,8 +449,21 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate = 0.0001,
         
         return parameters
 
-mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+parser = argparse.ArgumentParser()
+  # environment variable when name starts with $
+parser.add_argument('--data_dir', type=str, default='$DATA_DIR', help='Directory with data')
+parser.add_argument('--result_dir', type=str, default='$RESULT_DIR', help='Directory with results')
+parser.add_argument('--train_images_file', type=str, default='train-images-idx3-ubyte.gz', help='File name for train images')
+parser.add_argument('--train_labels_file', type=str, default='train-labels-idx1-ubyte.gz', help='File name for train labels')
+parser.add_argument('--test_images_file', type=str, default='t10k-images-idx3-ubyte.gz', help='File name for test images')
+parser.add_argument('--test_labels_file', type=str, default='t10k-labels-idx1-ubyte.gz', help='File name for test labels')
+parser.add_argument('--training_iters', type=int, default=1000, help='Number of training iterations')
+parser.add_argument('--batch_size', type=int, default=100, help='Training batch size')
 
+FLAGS, unparsed = parser.parse_known_args()
+
+
+mnist = read_data_sets(train_images_file, train_labels_file, test_images_file, test_labels_file, one_hot=False)
 train_data = mnist.train.images # Returns np.array
 train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
 eval_data = mnist.test.images # Returns np.array
